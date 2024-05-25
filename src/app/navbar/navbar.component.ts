d2da6935f1e67dc1de9a99c906600835f4333207
ng-bilel <bilel.feki.92@gmail.com>
2024-05-25 08:18:29 +0100

    feat(shared): add navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {
+  menuOpen = true;
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+  navItems = [
+    { name: 'SIGN IN | REGISTER', icon: 'fas fa-user' },
+    { name: '  WHICH LIST', icon: 'fas fa-heart' },
+  ];
+}
