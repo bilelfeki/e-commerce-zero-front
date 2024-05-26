b8d26cee2950405f3b2a217af058e624a519abab
ng-bilel <bilel.feki.92@gmail.com>
2024-05-26 23:40:46 +0100

    feat(shared): add product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SwiperModule } from 'swiper/angular';
 import { SwiperComponent } from './swiper/swiper.component';
+import { ProductComponent } from './product/product.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SwiperComponent
+    SwiperComponent,
+    ProductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  elements=[1,2,3,4,5,5,5,5,5,5,5,5,5,5]
+  titleProduct='hello'
+}
