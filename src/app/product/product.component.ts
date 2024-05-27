20124a91f40388610b22da442e441b82770003a1
ng-bilel <bilel.feki.92@gmail.com>
2024-05-27 22:59:02 +0100

    feat(shared): make product dynamic + add logo

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,11 +1,270 @@
 import { Component } from '@angular/core';
 
+export interface ProductDetails{
+  title:string;
+  description?:string;
+  price:number;
+  rate:number
+  imageSource:string
+}
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  elements=[1,2,3,4,5,5,5,5,5,5,5,5,5,5]
-  titleProduct='hello'
+  rateIterator=Array.from(Array(5).keys());
+  productDetails:ProductDetails[]=[{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  },{
+    title:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+    description:"",
+    price:599,
+    rate:3,
+    imageSource:"https://www.tunisianet.com.tn/241596-large/tablette-graphique-xp-pen-deco-fun-xs.jpg"
+  }]
 }
