5db29b27043d9121472677668ccb4cc40d5da99f
ng-bilel <bilel.feki.92@gmail.com>
2024-05-28 14:16:08 +0100

    refactor(shared): refactor footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface footer{
+  companyName:string,
+  linkList:string[],
+  onClickLogoLink:string,
+  logImageLink:string;
+}
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-  companyName='Shopnext'
+  footer= {
+    companyName:'shopnext',
+    linkList:['About','Privacy Policy','Licensing','Contact'],
+    onClickLogoLink:'https://flowbite.com/',
+    logImageLink:'../../assets/logo-custom.png'
+  }
 }
